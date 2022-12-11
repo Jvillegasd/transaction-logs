b0908daa2974f87c23dfe1214422b09466b71cb6
Jvillegasd <johnnyvillegaslrs@gmail.com>
2022-12-10 19:26:22 -0500

    feat: :sparkes: Added some doc

--- src/importer/importer.py
+++ src/importer/importer.py
@@ -12,9 +12,18 @@ class DataImporter:
         self.pwd = os.path.abspath(os.path.dirname(__file__))
 
         self.seeds: dict[str, dict] = self._load_seeds()
-        self.import_order: dict = self._load_import_order()
+        self.import_order: list[str] = self._load_import_order()
 
     def _load_seeds(self) -> dict[str, dict]:
+        """This method loads seeds JSON files stored in their
+        folder for in-memory consulting.
+
+        Returns:
+            -   dict[str, dict] = A dict with the seeds
+            stored in the specified folder. Where their keys represents
+            model's name and their value the seeds.
+        """
+
         seeds: dict[str, dict] = {}
         seeds_dir = os.path.abspath(os.path.join(self.pwd, 'seeds'))
         seeds_files = os.listdir(seeds_dir)
@@ -23,11 +32,20 @@ class DataImporter:
             file = os.path.abspath(os.path.join(seeds_dir, file_name))
 
             with open(file, 'r') as f:
-                seeds[file_name[:-5]] = json.load(f)
+                seed_dict = json.load(f)
+                seeds[seed_dict['model_name']] = seed_dict
 
         return seeds
 
-    def _load_import_order(self) -> dict:
+    def _load_import_order(self) -> list[str]:
+        """This method loads the import order list stored
+        in JSON file and it's used for loads all models in the right
+        order.
+
+        Returns:
+            -   list[str] = List of model names.
+        """
+
         import_order: dict = {}
         order_dir = os.path.abspath(
             os.path.join(self.pwd, 'import_order.json')
