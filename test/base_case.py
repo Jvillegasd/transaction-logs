6780756549da38de791a6004511cf7bc58174ef9
Jvillegasd <johnnyvillegaslrs@gmail.com>
2022-12-11 13:53:33 -0500

    test: :test_tube: Tests started

--- test/base_case.py
+++ test/base_case.py
@@ -1,2 +1,20 @@
+from src.importer.importer import DataImporter
+from src.connection.database import DataAccessLayer
+
+
 class BaseCase:
-    pass
+
+    @classmethod
+    def setup_class(cls):
+        cls._import_api()
+        cls._dal = DataAccessLayer()
+        cls.importer = DataImporter()
+        cls.importer.clear_all_models()
+
+    @classmethod
+    def teardown_class(clss):
+        pass
+
+    @classmethod
+    def _import_api(cls):
+        pass
---
+++ test/importer/seed_loading.py
@@ -0,0 +1,7 @@
+from test.base_case import BaseCase
+
+
+class TestImporterSeeds(BaseCase):
+
+    def setup_class(self):
+        super().setup_class()
