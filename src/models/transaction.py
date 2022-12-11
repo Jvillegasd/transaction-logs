b2efb393c55269574a339d544bd1e948f4d5d3d0
Jvillegasd <johnnyvillegaslrs@gmail.com>
2022-12-10 21:32:23 -0500

    feat: :sparkles: Added seeds

---
+++ src/models/transaction.py
@@ -0,0 +1,39 @@
+import enum
+
+from src.models.base import BaseModel
+
+from sqlalchemy import (
+    Enum,
+    Column,
+    String,
+    Float,
+    ForeignKey
+)
+from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.postgresql import UUID
+
+
+class TransactionType(enum.Enum):
+    deposit='deposit'
+    withdraw='withdraw'
+    expense='expense'
+
+
+class Transaction(BaseModel):
+    __tablename__ = 'transactions'
+
+    user_id = Column(UUID(as_uuid=True), ForeignKey('users.id'))
+    name = Column(String, nullable=False)
+    merchant = Column(String)
+    transaction_type = Column(
+        Enum(TransactionType),
+        default=TransactionType.deposit,
+        nullable=False
+    )
+    amount = Column(Float, nullable=False)
+
+    user = relationship(
+        'User',
+        back_populates='transactions',
+        viewonly=True
+    )
---
+++ src/models/user.py
@@ -0,0 +1,21 @@
+from src.models.base import BaseModel
+
+from sqlalchemy import (
+    Column,
+    String
+)
+from sqlalchemy.orm import relationship
+
+
+class User(BaseModel):
+    __tablename__ = 'users'
+
+    name = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+
+    transactions = relationship(
+        'Transaction',
+        back_populates='user',
+        viewonly=True
+    )
