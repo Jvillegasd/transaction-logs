a995691f2965723e7dfc8e07cad57b9f6a05f64a
Jvillegasd <johnnyvillegaslrs@gmail.com>
2022-12-12 00:52:04 -0500

    feat: :sparkles: DAL loaded in context

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os
 
-from src import create_app, dal
+from src import create_app, get_dal
 
 port = os.getenv('PORT', 5001)
 app = create_app(config_name=os.getenv('APP_ENV', 'dev'))
 
 
 if __name__ == '__main__':
-    dal.create_tables()
+    get_dal().create_tables()
     app.run(host='0.0.0.0', port=port)
--- src/__init__.py
+++ src/__init__.py
@@ -2,14 +2,41 @@ from src.controllers import home_api
 from src.config.app_config import config_by_name
 from src.connection.database import DataAccessLayer
 
-from flask import Flask
+from flask import Flask, g
 
-dal = DataAccessLayer()
+
+def get_dal() -> DataAccessLayer:
+    """Store a Data Access Layer instance in Flask
+    Application context for use this across requests.
+
+    Returns:
+        -   DataAccessLayer
+    """
+
+    if 'dal' not in g:
+        g.dal = DataAccessLayer()
+
+    return g.dal
 
 
 def create_app(config_name: str) -> Flask:
+    """Creates a Flask application and loads configurations
+    depending on the provided environment. After the application
+    is created, current application context is pushed for 
+    use loaded varibles outside context.
+
+    Args:
+        -   config_name: str = Enviroment configuration
+
+    Returns:
+        -   Flask = Flask application
+    """
+
     app = Flask(__name__)
     app.config.from_object(config_by_name[config_name])
+
+    # Push the Application context
+    app.app_context().push()
 
     # Blueprints
     app.register_blueprint(home_api, url_prefix='/api')
