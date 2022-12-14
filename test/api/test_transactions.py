906f24699a219c414e36c930862e8848e5f0375a
Jvillegasd <johnnyvillegaslrs@gmail.com>
2022-12-13 21:07:39 -0500

    test: :test_tube: TDD for user balance implemented

--- test/api/test_transactions.py
+++ test/api/test_transactions.py
@@ -21,3 +21,17 @@ class TestApiTransaction(BaseCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertGreater(len(response_json['records']), 0)
+        self.assertIn('cursor', response_json)
+
+    def test_get_user_balance(self):
+        data = self.users[0]
+        with self.client.session_transaction() as session:
+            session['user_id'] = data['id']
+
+        response = self.client.get('/api/transactions/balance')
+        response_json = response.get_json()
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('balance', response_json)
+        self.assertEqual(type(response_json['balance']), float)
+        self.assertGreaterEqual(response_json['balance'], 0)
